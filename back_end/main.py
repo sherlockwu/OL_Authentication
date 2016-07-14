ecc8a4665bdc43821d804a17980c0901eec948e5
Kevin@Ubuntu <wukanustc@gmail.com>
2016-07-14 15:22:13 +0800

    add the organization for backend

---
+++ back_end/main.py
@@ -0,0 +1,7 @@
+# main logic for back-end processing
+
+# listening ?
+
+# call for OL processing ?
+
+# reply to front_end
--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -1,1 +1,4 @@
 # this is for generating lambda application
+
+# handler
+function
