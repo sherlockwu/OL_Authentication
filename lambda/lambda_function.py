c0dc7d82b4521fde35d9601757e894f4b3b0348f
Kevin@Ubuntu <wukanustc@gmail.com>
2016-07-15 09:10:06 +0800

    organize the lambda function

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -1,4 +1,61 @@
 # this is for generating lambda application
+import ???
+
+
+
+# first log-in
+def init(conn, event):
+    # get the account and the input_pw
+    usr_accounts = ???
+    input_pw = ???
+    # check the accounts
+        # connect to the database?   RethinkDB
+
+        # Select
+        true_pw = select ... usr_accounts  # ???
+            #account not exist
+            if true_pw == None:
+                return "Non-exist Account"
+        if (true_pw == input_pw ):
+            # cal fbid
+            fbid = cal_fbid()
+            return ["Successfully log-in", fbid]
+        else:
+            return "false password"
+
+
+# check log-in status (using fbid?)
+def check(conn, event):
+    # get fbid
+    fbid = ... ???
+    # check fbid    database?
+
+    # return status
+
+
+# exit log-in
+def exit_login(conn, event):
+    # invalidate fbid
+
+
 
 # handler
-function
+def handler(conn,event):  # conn??
+    fn = {'init':   init,
+          'check':  check
+          'exit':   exit_login}.get(event['op'], None)  #??
+
+    if fn!=None:
+
+       result = fn(conn, event)
+    # first log-in
+
+
+    # check log-in status
+
+    # exit log-in
+        return {'result': result}
+    else:
+        # bad option
+        return("bad option")   # ?
+
