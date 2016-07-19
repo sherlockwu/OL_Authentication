6153cbc69f4e72e73eb484efd3cf0f5469635b82
Kevin@Ubuntu <wukanustc@gmail.com>
2016-07-19 13:58:50 +0800

    finish the writing of the test html

--- sdk/main.js
+++ sdk/main.js
@@ -1,18 +1,55 @@
-var account;
-var passwd;
+function lambda_post(data, callback){
+    // url  ????
+    var url = "192.168.0.1:8080";
 
-function login(){
-    <!--create the data-->
+    // post
+    $.ajax({
+        type: "POST",
+        url : url,
+        contentType: "application/json; charset=utf-8",
+        data: JSON.stringify(data),
+        dataType: "json",
+        success: callback,
+        failure: function(error){
+            $.("#submit").html("Error: " + error);
+        }
+    });
 
-    JSON_Account = "{";
-
-    account = $("#account")[0];
-    passwd = $("#passwd")[0];
-
-    JSON_Account += " \"account :\" " + account + "\"passwd : \"" + passwd + "}";
-    <!--JSON Style-->
-
-
-    <!--send out the data-->
 
 }
+
+function login(){
+    // get the data
+    var msg_account;
+    var msg_passwd;
+    msg_account = $("#account").val();
+    msg_passwd = $("#passwd").val();
+    // post the data
+    alert("We are using your account: " + msg_account + " passwd: " + msg_passwd + "to log in.");
+    /*
+    lambda_post(
+        {"op" : "login", "account" : msg_account, "passwd" : msg_passwd},
+        function(data){
+            $("#account").val("");
+            $("#passwd").val("");
+        }
+    );
+    */
+}
+
+
+function main(){
+    // log-in
+    $("#passwd").val("");
+    $("#passwd").keypress(function(e){
+        if (e.keyCode==13){
+            $("#submit").click();
+        }
+    });
+    $("#submit").click(login);
+}
+
+
+$(document).ready(function(){
+     main();
+})
