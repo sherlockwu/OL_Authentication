1cb37ab3cba9ceb614ff3b071ee499af0f41bb1d
Kevin@Ubuntu <wukanustc@gmail.com>
2016-07-22 21:57:08 +0800

    write out the first version lambda handler for log-in, getting fbid

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -2,27 +2,37 @@
 import traceback
 import rethinkdb as r
 
+DB = 'authentication'  # authentication database
+TB = 'log_in'          # account table
+AC = 'account'         # column
+PW = 'password'        # column
+ID = 'fbid'            # column
 
+def cal_fbid(usr_object):
+    # cal fbid algorithm   ????
+    fbid = usr_object[AC] + usr_object[PW]
+    return fbid
 
 # first log-in
 def login(conn, event):
     # get the account and the input_pw
-    usr_accounts = ???
-    input_pw = ???
-    # check the accounts
-        # connect to the database?   RethinkDB
+    usr_accounts = event['account']
+    input_pw = event['passwd']
+# check the accounts
+    # connect to the database? conn: RethinkDB
+    # filter out the guest through account (how to realize this)
+    cursor = r.table(TB).filter({AC: usr_accounts}).run(conn)
+    #account not exist
+    if cursor == None:
+        return {'error': 'Non-exist Account'}
 
-        # Select
-        true_pw = select ... usr_accounts  # ???
-            #account not exist
-            if true_pw == None:
-                return "Non-exist Account"
-        if (true_pw == input_pw ):
+    for row in cursor:
+        if (row[PW] == input_pw ):
             # cal fbid
-            fbid = cal_fbid()
-            return ["Successfully log-in", fbid]
+            fbid = cal_fbid(row)
+            return {'result': fbid}
         else:
-            return "false password"
+            return {'error': 'wrong password'}
 
 # check log-in status (using fbid?)
 def check(conn, event):
@@ -31,12 +41,12 @@ def check(conn, event):
     # check fbid    database?
 
     # return status
-
+    return ''
 
 # exit log-in
 def exit_login(conn, event):
     # invalidate fbid
-
+    return ''
 
 
 # handler
