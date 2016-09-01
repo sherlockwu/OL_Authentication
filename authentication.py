be04ed06d3cd4b45e5195d58aefa65b6f752c2c5
Kevin@Ubuntu <wukanustc@gmail.com>
2016-09-01 12:18:07 +0800

    finished the log-out function

--- authentication.py
+++ authentication.py
@@ -1,5 +1,6 @@
 # this is for generating lambda application
 import traceback
+import datetime
 import rethinkdb as r
 
 DB = 'authentication'  # authentication database
@@ -10,7 +11,7 @@ ID = 'fbid'            # column
 
 def cal_fbid(usr_object):
     # cal fbid algorithm   TODO
-    fbid = usr_object[AC] + usr_object[PW]
+    fbid = usr_object[AC] + str(datetime.datetime.now().strftime("%Y%m%d%H%M%S"))
     return fbid
 
 # first log-in
@@ -24,24 +25,24 @@ def login(conn, event):
     # filter out the guest through account (how to realize this)
     cursor = r.db(DB).table(TB).filter({AC: usr_accounts}).run(conn)
     #account not exist
-    #if cursor == None:
-    #    return 'Non-exist Account'
 
     for row in cursor:
         if (row[PW] == input_pw ):
             # cal fbid
             fbid = cal_fbid(row)
-            return fbid
+            # TODO insert the fbid into database
+            r.db(DB).table(TB).filter({AC: usr_accounts}).update({ID: fbid}).run(conn)
+            return {'status': 'login successfully', 'fbid': fbid}
         else:
-            return 'wrong password'
-    return 'Non-exist Account'
+            return {'status': 'wrong password'}
+    return {'status': 'Non-exist Account'}
 def add(conn, event):
     # get the account
     usr_accounts = event['account']
     usr_pw = event['passwd']
     # store into the rethinkdb
     r.db(DB).table(TB).insert({AC: usr_accounts, PW: usr_pw}).run(conn)
-    return 'sucessfully added!!!'
+    return {'status': 'sucessfully added!'}
 # check log-in status (using fbid?) TODO
 def check(conn, event):
     # get fbid
@@ -49,12 +50,26 @@ def check(conn, event):
     # check fbid    database?
 
     # return status
-    return ''
+    return {'status': 'cannot check right now'}
 
 # exit log-in
-def exit_login(conn, event):
+def logout(conn, event):
     # invalidate fbid
-    return ''
+    usr_accounts = event['account']
+    input_fbid = event['fbid']
+
+    # find the account
+    cursor = r.db(DB).table(TB).filter({AC: usr_accounts}).run(conn)
+    # check the fbid
+    for row in cursor:
+        if (row[ID] == input_fbid):
+            fbid = cal_fbid(row)
+            # TODO renew the fbid of this row
+            r.db(DB).table(TB).filter({AC: usr_accounts}).update({ID: fbid}).run(conn)
+            return {'status': 'logout successfully'}
+    # renew the fbid
+
+    return {'status': 'cannot exit right now'}
 
 
 # handler
@@ -62,13 +77,15 @@ def handler(conn,event):
     fn = {'login':  login,
           'add'  :  add,
           'check':  check,
-          'exit' :  exit_login}.get(event['op'], None)
+          'logout' : logout}.get(event['op'], None)
+
 
     print (fn)
     if fn!=None:
         try:
             result = fn(conn, event)
-            return {'result': result}
+            # return {'result': result}
+            return result
         except Exception:
             return {'error': traceback.format_exc()}
     else:
--- static/main.js
+++ static/main.js
@@ -1,4 +1,6 @@
 var config;
+var fbid = "0";
+var msg_account;
 
 function lambda_post(data, callback){
     // url
@@ -23,9 +25,9 @@ function lambda_post(data, callback){
 }
 
 function login(){
-    // get the data
-    var msg_account;
     var msg_passwd;
+    //try to not store the passwd in script
+    // get the data
     msg_account = $("#account").val();
     msg_passwd = $("#passwd").val();
     // post the data
@@ -34,9 +36,45 @@ function login(){
     lambda_post(
         {"op" : "login", "account" : msg_account, "passwd" : msg_passwd},
         function(data){
-            alert(JSON.stringify(data));
+            //alert the log-in status
+            status = data['status'];
+            alert(status);
+            if (status == "login successfully"){
+            // store the fbid
+                fbid = data['fbid'];
+                alert(fbid);
+                $("#login_status").html("Logged in as "+msg_account);
+                $("#log_out").attr("style", "display: block;");
+            }
             $("#account").val("");
             $("#passwd").val("");
+            // status changed and present a log-out button
+        }
+    );
+
+}
+
+function store_habit(){
+
+    // TODO store the habit into logged account database
+    alert("The habit has been stored!");
+
+}
+
+function logout(){
+    //send logout info (just for invalidate the fbid)
+    lambda_post(
+        {"op": "logout", "account" : msg_account, "fbid" : fbid},
+        function(data){
+            alert("Successfully log-out");
+            //change the log-in status
+            $("#login_status").html("logged out");
+
+            //reset the fbid
+            fbid = "";
+
+            //hide the log-out button
+            $("#log_out").attr("style", "display: none;");
         }
     );
 
@@ -59,6 +97,22 @@ function main(){
                 }
             });
             $("#submit").click(login)
+
+            // habit "enter"
+            $("#habit").keypress(function(e){
+                // TODO 1. check the login status 2. insert habits to the database then
+                if (e.keyCode == 13){
+                    // check login status
+                    if (fbid == "0"){
+                        alert("Please Log-in first!");
+                    } else{
+                        store_habit();
+                    }
+                }
+            });
+
+            // log-out
+            $("#log_out").click(logout)
         }
         ).fail(function(jqxhr, textStatus, error){
             // direct refreshing
